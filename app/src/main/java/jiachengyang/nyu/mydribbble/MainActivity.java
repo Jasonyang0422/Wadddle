cb7ac8dad938c758d7d375f37e6c6dff87781137
Jiacheng Yang <jy1964@nyu.edu>
2017-04-11 01:32:37 -0400

    built navigation drawer without header

--- app/src/main/java/jiachengyang/nyu/mydribbble/MainActivity.java
+++ app/src/main/java/jiachengyang/nyu/mydribbble/MainActivity.java
@@ -1,21 +1,115 @@
 package jiachengyang.nyu.mydribbble;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import jiachengyang.nyu.mydribbble.view.shot_list.ShotListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view) NavigationView navigationView;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fragment_container, ShotListFragment.newInstance())
-                .commit();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //build back button
+        getSupportActionBar().setHomeButtonEnabled(true); //build back button
+
+        setupDrawer();
+
+        if(savedInstanceState == null) { //防止屏幕倒过来产生多个fragment
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ShotListFragment.newInstance())
+                    .commit();
+        }
+    }
+
+    //create animated hamburg icon
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    //create animated hamburg icon
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,          /* DrawerLayout object */
+                R.string.open_drawer,         /* "open drawer" description */
+                R.string.close_drawer         /* "close drawer" description */
+        );
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.isChecked()) {
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+
+                Fragment fragment = null;
+                switch(item.getItemId()) {
+                    case R.id.navigation_view_menu_home:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle("MyDribbble");
+                        break;
+                    case R.id.navigation_view_menu_likes:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle("Likes");
+                        break;
+                    case R.id.navigation_view_menu_buckets:
+                        Toast.makeText(MainActivity.this, "Welcome to Buckets!", Toast.LENGTH_LONG).show();
+                        setTitle("Buckets");
+                        break;
+                }
+
+                drawerLayout.closeDrawers();
+
+                if(fragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                    return true;
+                }
+
+                return true;
+            }
+        });
     }
 }
